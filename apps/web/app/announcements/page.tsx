67ef0a178701791e71732a92a285b808c9f87a09
Artaius <36020251+xartaiusx@users.noreply.github.com>
2026-07-12 10:00:12 -0700

    Make static content routes build-time only

--- apps/web/app/announcements/page.tsx
+++ apps/web/app/announcements/page.tsx
@@ -4,6 +4,5 @@ import { metadataFor } from "@/components/public-pages/metadata";
 import { AnnouncementsPage } from "@/components/public-pages/pages";
 
 export const metadata = metadataFor("announcements");
-export const revalidate = 3600;
 
 export default AnnouncementsPage;
--- apps/web/app/events/page.tsx
+++ apps/web/app/events/page.tsx
@@ -6,6 +6,5 @@ import { metadataFor } from "@/components/public-pages/metadata";
 import { EventsPage } from "@/components/public-pages/pages";
 
 export const metadata = metadataFor("events");
-export const revalidate = 3600;
 
 export default EventsPage;
